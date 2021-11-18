405f07b03d03376754d54ee874a5563e7690831d
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2021-11-18 22:09:08 +0330

    chore: add dev config

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: {
+    // https://tailwindcss.com/
+    tailwindcss: {},
+
+    // https://github.com/csstools/postcss-preset-env
+    'postcss-flexbugs-fixes': {
+      'postcss-preset-env': {
+        stage: 3,
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        features: {
+          'custom-properties': false,
+        },
+      },
+    },
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  darkMode: 'media', // or false or 'class'
+
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // ...
+  ],
+}
